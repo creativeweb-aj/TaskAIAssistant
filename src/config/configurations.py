1458c52d68384657cdccbfa703b9ef4e3473d46b
Ajay Kumar Sharma <48996332+creativeweb-aj@users.noreply.github.com>
2024-02-27 01:05:09 +0530

    Update configurations.py

--- src/config/configurations.py
+++ src/config/configurations.py
@@ -10,4 +10,4 @@ SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS'
 # Upload media files folders
 UPLOAD_FOLDER = os.path.join(os.path.abspath('media') + '/uploads')
 
-OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY', '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')
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY', '')
